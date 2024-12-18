6f11c7c58ab7bdf011f688842796599781dbb40f
Bobakek <165632522+Bobakek@users.noreply.github.com>
2024-12-18 21:18:22 +0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -11,18 +11,17 @@ from starlette.responses import HTMLResponse
 
 load_dotenv()
 
-# Путь к базе данных
+
 DB_PATH = "./bot/storage/db.json"
 DB_DIR = "./bot/storage"
 
-# Telegram Bot API токен
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Инициализация Telegram бота
 bot = Bot(token=BOT_TOKEN)
 
 
-# Убедимся, что файл базы данных существует
+
 def ensure_db_exists():
     if not os.path.exists(DB_DIR):
         os.makedirs(DB_DIR)
@@ -31,21 +30,21 @@ def ensure_db_exists():
             json.dump({}, f)
 
 
-# Функция для загрузки базы данных
+
 def load_db():
     ensure_db_exists()
     with open(DB_PATH, "r") as f:
         return json.load(f)
 
 
-# Инициализация FastAPI
+
 app = FastAPI()
 
-# Настройка Jinja2Templates для рендера HTML
+
 templates = Jinja2Templates(directory="templates")
 
 
-# Эндпоинт для получения списка файлов
+
 @app.get("/files")
 async def get_files():
     db = load_db()
@@ -56,11 +55,11 @@ async def get_files():
     for name, data in db.items():
         file_id = data["file_id"]
         try:
-            # Получаем информацию о файле
+            
             file_info = await bot.get_file(file_id)
-            file_size = file_info.file_size  # Размер файла в байтах
+            file_size = file_info.file_size  
 
-            # Проверка, не превышает ли файл лимит
+            
             if file_size > 20 * 1024 * 1024:  # 20 МБ
                 file_size = "Too Large (over 20 MB)"
 
@@ -72,7 +71,7 @@ async def get_files():
     return {"files": files}
 
 
-# Эндпоинт для получения ссылки на скачивание файла
+
 @app.get("/files/{file_name}")
 async def get_file_download_link(file_name: str):
     db = load_db()
@@ -81,7 +80,7 @@ async def get_file_download_link(file_name: str):
 
     file_id = db[file_name]["file_id"]
 
-    # Получаем путь к файлу от Telegram
+    
     try:
         file = await bot.get_file(file_id)
         file_path = file.file_path
@@ -97,7 +96,7 @@ def save_db(data):
     with open(DB_PATH, "w") as f:
         json.dump(data, f, indent=4)
 
-# Эндпоинт для удаления файла
+# Удал.файла
 @app.delete("/files/{file_name}")
 async def delete_file(file_name: str):
     db = load_db()
@@ -112,7 +111,7 @@ async def delete_file(file_name: str):
 
     return JSONResponse(content={"message": f"File '{file_name}' successfully deleted."})
 
-# Эндпоинт для отображения HTML-страницы
+# отображение HTML-страницы
 @app.get("/", response_class= HTMLResponse)
 async def serve_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
